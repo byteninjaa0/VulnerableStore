c096daa6d862d2b7c44d196862a9739e5ec3888d
sohail <sksohail.swaraj@gmail.com>
2025-05-09 07:47:07 +0530

    images added

--- db_setup.py
+++ db_setup.py
@@ -79,14 +79,14 @@ c.execute("INSERT INTO users (username, password, email, address, is_admin) VALU
 
 # Products
 products = [
-    ('Smartphone', 'Latest model with high-resolution camera', 699.99, 50, '/static/img/smartphone.jpg'),
+    ('Smartphone', 'Latest model with high-resolution camera', 699.99, 50, 'https://www.pngplay.com/wp-content/uploads/7/Apple-IPhone-Mobile-PNG-HD-Quality.png'),
     ('Laptop', 'Powerful laptop for work and gaming', 1299.99, 30, 'https://images.pexels.com/photos/538969/pexels-photo-538969.jpeg?cs=srgb&dl=computer-laptop-technology-538969.jpg&fm=jpg'),
-    ('Headphones', 'Noise-cancelling wireless headphones', 199.99, 100, '/static/img/headphones.jpg'),
-    ('Smartwatch', 'Track your fitness and get notifications', 249.99, 45, '/static/img/smartwatch.jpg'),
-    ('Tablet', '10-inch screen with long battery life', 399.99, 60, '/static/img/tablet.jpg'),
-    ('Gaming Console', 'Next-gen gaming experience', 499.99, 25, '/static/img/console.jpg'),
-    ('Camera', 'DSLR with 24MP sensor', 799.99, 20, '/static/img/camera.jpg'),
-    ('Bluetooth Speaker', 'Portable speaker with great sound', 129.99, 80, '/static/img/speaker.jpg')
+    ('Headphones', 'Noise-cancelling wireless headphones', 199.99, 100, 'https://c.pxhere.com/photos/5b/11/headphones_philips_headphones_music_listening_audio_audiophile_sound_earphones-1097908.jpg!d'),
+    ('Smartwatch', 'Track your fitness and get notifications', 249.99, 45, 'https://m.media-amazon.com/images/I/71biAGp1unL.jpg'),
+    ('Tablet', '10-inch screen with long battery life', 399.99, 60, 'https://www.gizmotimes.com/wp-content/uploads/2018/04/Alcatel-A310-Tablet.jpeg'),
+    ('Gaming Console', 'Next-gen gaming experience', 499.99, 25, 'https://i5.walmartimages.com/asr/1577430e-3038-4cbf-a545-6bf8cbfb165e_1.4221efd3f903a904753156e2441d66ef.jpeg'),
+    ('Camera', 'DSLR with 24MP sensor', 799.99, 20, 'https://tse4.mm.bing.net/th?id=OIP.ffcgI_zR5I7hHl6qGRS_WwHaGH&pid=Api&P=0&h=220'),
+    ('Bluetooth Speaker', 'Portable speaker with great sound', 129.99, 80, 'https://i5.walmartimages.com/asr/2b6b6154-aab6-4f96-a3d7-3c385b3b36fe_1.47e5371360dc22cee9acea11190ddd6e.jpeg')
 ]
 
 for product in products:
